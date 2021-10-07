eb647fce707484611a1d0e61f6a0e145c3d9283b
xxxxxsty <91723124+xxxxxsty@users.noreply.github.com>
2021-10-07 15:20:15 +0900

    Update test.py
    star

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 def findMax(a,b,c):
     if a>b:
-        biggest=a
+        big=a
     else:
-        biggest=b
+        big=b
     if c>big:
-        biggest=c
-    return biggest
+        big=c
+    return big
 a= int (input("첫 번째 숫자 입력: "))
 b= int (input("두 번째 숫자 입력: "))
 c= int (input("세 번째 숫자 입력: "))
 biggest = findmax(a,b,c)
-print(a,b,c,"중 가장 큰 수는",biggest,"입니다.")
+print(a,b,c,"중 가장 큰 수는",big,"입니다.")
